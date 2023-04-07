66acf8b7829d9628cbb174bf3f64639dc7d6796b
Gladys TISBA <gladys.tisba@ens.uvsq.fr>
2023-04-07 14:03:34 +0200

    def rotation fourmi

--- Fourmi de Langton final.py
+++ Fourmi de Langton final.py
@@ -20,9 +20,31 @@ previous_steps=[]
 
 #Permet le changement de la couleur de la case en fonction de la position de la fourmi
 def changement_couleur():
-   global color, fourmi_row, fourmi_col, direction, previous_steps
-   color = grid[fourmi_row][fourmi_col]
+   global couleur, fourmi_row, fourmi_col, direction, previous_steps
+   couleur = grid[fourmi_row][fourmi_col]
    #permet de récuperer la position case actuelle (pour annuler)
-   previous_steps.append((fourmi_row,fourmi_col,direction,color))
+   previous_steps.append((fourmi_row,fourmi_col,direction,couleur))
    print(previous_steps)
-   grid[fourmi_row][fourmi_col] = BLACK if color == WHITE else WHITE
+   grid[fourmi_row][fourmi_col] = BLACK if couleur == WHITE else WHITE
+
+def rotation_fourmi():
+    global direction
+    changement_couleur()
+    if couleur == WHITE:
+        if direction == "up":
+            direction = "right"
+        elif direction == "right":
+            direction = "down"
+        elif direction == "down":
+            direction = "left"
+        elif direction == "left":
+            direction = "up"
+    elif couleur == BLACK:
+        if direction == "up":
+            direction = "left"
+        elif direction == "left":
+            direction = "down"
+        elif direction == "down":
+            direction = "right"
+        elif direction == "right":
+            direction = "up"
